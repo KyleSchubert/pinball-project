342e50005403624ff098e9c57ba3f63b4ef2765e
Shrifli <jedikyle5@gmail.com>
2022-11-09 14:14:16 -0500

    - Removed tests and simplified a function
    - The test rectangle that had the image of the penguin is now removed.
    - The createPinballBoard function was split into 2 functions
    - And then I also just moved that "START SUGGESTED CODE FROM ->" section to the bottom because it gets in the way

--- pinball/core/src/com/pinball/mygame/MyGame.java
+++ pinball/core/src/com/pinball/mygame/MyGame.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -16,27 +15,10 @@ import com.codeandweb.physicseditor.PhysicsShapeCache;
 public class MyGame extends ApplicationAdapter {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	Texture testImage;
-	private Rectangle testRectangle;
 	World world;
     ExtendViewport viewport;
     Sprite pinball;
     static final float SCALE_FACTOR = 0.05f;
-    // START SUGGESTED CODE FROM -> https://www.codeandweb.com/physicseditor/tutorials/libgdx-physics
-    static final float STEP_TIME = 1f / 60f;
-    static final int VELOCITY_ITERATIONS = 6;
-    static final int POSITION_ITERATIONS = 2;
-
-    float accumulator = 0;
-    private void stepWorld() {
-        float delta = Gdx.graphics.getDeltaTime();
-        accumulator += Math.min(delta, 0.25f);
-        if (accumulator >= STEP_TIME) {
-            accumulator -= STEP_TIME;
-            world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
-        }
-    }
-    // END SUGGESTED CODE FROM -> https://www.codeandweb.com/physicseditor/tutorials/libgdx-physics
     Box2DDebugRenderer debugRenderer;
     PhysicsShapeCache physicsBodies;
     Body pinballPhysicsSpot;
@@ -52,16 +34,9 @@ public class MyGame extends ApplicationAdapter {
         physicsBodies = new PhysicsShapeCache("physics.xml");
 
 		batch = new SpriteBatch();
-		testImage = new Texture("pepe.png");
 
 		camera = new OrthographicCamera();
         viewport = new ExtendViewport(800*SCALE_FACTOR, 900*SCALE_FACTOR, camera);
-
-		testRectangle = new Rectangle();
-		testRectangle.x = 300*SCALE_FACTOR;
-		testRectangle.y = 500*SCALE_FACTOR;
-		testRectangle.width = 30*SCALE_FACTOR;
-		testRectangle.height = 30*SCALE_FACTOR;
 
         Texture pinballTexture = new Texture("pinball v2.png"); // so I don't need to .dispose() this one?
         pinball = new Sprite(pinballTexture, 0, 0, 32, 32);
@@ -78,11 +53,8 @@ public class MyGame extends ApplicationAdapter {
 		ScreenUtils.clear(0, 0, 0, 1);
 
         stepWorld();
-        testRectangle.x += 1*SCALE_FACTOR;
-        testRectangle.y -= 1*SCALE_FACTOR;
 
 		batch.begin();
-		batch.draw(testImage, testRectangle.x, testRectangle.y);
         Vector2 pinballPosition = pinballPhysicsSpot.getPosition();
         float pinballDegrees = (float) Math.toDegrees(pinballPhysicsSpot.getAngle()); // doesn't do anything to circles?
         drawSprite(pinball, pinballPosition.x, pinballPosition.y, pinballDegrees);
@@ -111,33 +83,45 @@ public class MyGame extends ApplicationAdapter {
     }
 
     private void createPinballBoard() {
+        pinballBoard = prepareWorldPart(pinballBoard, "pinball board");
+        pinballBoardPaddleArea1 = prepareWorldPart(pinballBoardPaddleArea1, "pinball board paddle area");
+        pinballBoardPaddleArea2 = prepareWorldPart(pinballBoardPaddleArea2, "pinball board paddle area2");
+    }
+
+    private Body prepareWorldPart(Body body, String physicsBodyName) {
+        if (body != null) {
+            world.destroyBody(body);
+        }
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.StaticBody;
-
-        if (pinballBoard != null) {
-            world.destroyBody(pinballBoard);
-        }
-        pinballBoard = physicsBodies.createBody("pinball board", world, bodyDef, SCALE_FACTOR, SCALE_FACTOR);
-        pinballBoard.setTransform(0, 0, 0);
-
-        if (pinballBoardPaddleArea1 != null) {
-            world.destroyBody(pinballBoardPaddleArea1);
-        }
-        pinballBoardPaddleArea1 = physicsBodies.createBody("pinball board paddle area", world, bodyDef, SCALE_FACTOR, SCALE_FACTOR);
-        pinballBoardPaddleArea1.setTransform(0, 0, 0);
-
-        if (pinballBoardPaddleArea2 != null) {
-            world.destroyBody(pinballBoardPaddleArea2);
-        }
-        pinballBoardPaddleArea2 = physicsBodies.createBody("pinball board paddle area2", world, bodyDef, SCALE_FACTOR, SCALE_FACTOR);
-        pinballBoardPaddleArea2.setTransform(0, 0, 0);
+        body = physicsBodies.createBody(
+                physicsBodyName,
+                world, bodyDef, SCALE_FACTOR, SCALE_FACTOR
+        );
+        body.setTransform(0, 0, 0);
+        return body;
     }
 
 	@Override
 	public void dispose () {
 		batch.dispose();
-		testImage.dispose();
         world.dispose();
         debugRenderer.dispose();
 	}
+    
+    // START SUGGESTED CODE FROM -> https://www.codeandweb.com/physicseditor/tutorials/libgdx-physics
+    static final float STEP_TIME = 1f / 60f;
+    static final int VELOCITY_ITERATIONS = 6;
+    static final int POSITION_ITERATIONS = 2;
+
+    float accumulator = 0;
+    private void stepWorld() {
+        float delta = Gdx.graphics.getDeltaTime();
+        accumulator += Math.min(delta, 0.25f);
+        if (accumulator >= STEP_TIME) {
+            accumulator -= STEP_TIME;
+            world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+        }
+    }
+    // END SUGGESTED CODE FROM -> https://www.codeandweb.com/physicseditor/tutorials/libgdx-physics but more was used
 }
